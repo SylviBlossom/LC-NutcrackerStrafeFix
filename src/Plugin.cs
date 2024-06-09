3aac6c5bcd51e6d80778f91eaf30a44e404796f0
SylviBlossom <sylviblossom@gmail.com>
2024-06-09 12:49:03 -0700

    Version 1.0.0

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,13 +1,56 @@
 ﻿using BepInEx;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
+using MonoMod.Utils;
+using System;
+using System.Reflection;
 
-namespace TemplateMod;
+namespace NutcrackerStrafeFix;
 
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
 	private void Awake()
 	{
+		new ILHook(
+			typeof(NutcrackerEnemyAI).GetMethod("AimGun", BindingFlags.Instance | BindingFlags.NonPublic).GetStateMachineTarget(),
+			NutcrackerEnemyAI_AimGun
+		);
+
 		// Plugin startup logic
 		Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+	}
+
+	private void NutcrackerEnemyAI_AimGun(ILContext il)
+	{
+		var cursor = new ILCursor(il);
+
+		if (!cursor.TryGotoNext(MoveType.After, instr => instr.MatchStfld<EnemyAI>("inSpecialAnimation")))
+		{
+			Logger.LogError("Failed IL hook for NutcrackerEnemyAI.AimGun @ First inSpecialAnimation assignment");
+			return;
+		}
+
+		cursor.Emit(OpCodes.Ldloc_1);
+		cursor.EmitDelegate<Action<NutcrackerEnemyAI>>(self =>
+		{
+			self.inSpecialAnimation = false;
+			self.updatePositionThreshold = 0.25f;
+		});
+
+		if (!cursor.TryGotoNext(MoveType.After, instr => instr.MatchStfld<EnemyAI>("inSpecialAnimation")))
+		{
+			Logger.LogError("Failed IL hook for NutcrackerEnemyAI.AimGun @ Last inSpecialAnimation assignment");
+			return;
+		}
+
+		cursor.Emit(OpCodes.Ldloc_1);
+		cursor.EmitDelegate<Action<NutcrackerEnemyAI>>(self =>
+		{
+			self.updatePositionThreshold = 1f;
+		});
+
+		Logger.LogInfo("Hooked successfully");
 	}
 }
--- src/PluginInfo.cs
+++ src/PluginInfo.cs
@@ -1,8 +1,8 @@
-namespace TemplateMod;
+namespace NutcrackerStrafeFix;
 
 public static class PluginInfo
 {
-	public const string PLUGIN_GUID = "TemplateMod";
-	public const string PLUGIN_NAME = "TemplateMod";
+	public const string PLUGIN_GUID = "moe.sylvi.NutcrackerStrafeFix";
+	public const string PLUGIN_NAME = "NutcrackerStrafeFix";
 	public const string PLUGIN_VERSION = "1.0.0";
 }
